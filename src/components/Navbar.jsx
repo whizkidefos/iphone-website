8be2d7d301e64f2b2b237fc86fa8aebd1bb78aad
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-04-18 22:48:22 +0100

    Navbar build

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                 ))}
             </div>
 
-            <div className="">
+            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
                 <img src={searchImg} alt="search" width={18} height={18} className="" />
                 <img src={bagImg} alt="search" width={18} height={18} className="" />
             </div>
