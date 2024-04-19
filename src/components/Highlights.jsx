eee4205167b52d401fe1affa5b040f11eddbd003
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-04-20 00:06:31 +0100

    Highlight component build

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,8 +1,33 @@
-
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { rightImg, watchImg } from "../utils";
 
 const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0, duration: 1, delay: 0.5});
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, delay: 0.5, stagger: 0.2});
+  }, []);
+  
   return (
-    <div>Highlights</div>
+    <section id="highlights" className="w-screen h-full overflow-hidden common-padding bg-zinc">
+      <div className="screen-max-width">
+        <div className="items-end justify-between w-full mb-12 md:flex">
+          <h1 className="section-heading" id="title">Get the highlights.</h1>
+
+          <div className="flex flex-wrap items-end gap-5">
+            <p className="link">
+              Watch the clip
+              <img src={watchImg} alt="play icon" className="ml-2" />
+            </p>
+            <p className="link">
+              Watch the event
+              <img src={rightImg} alt="play icon" className="ml-2" />
+            </p>
+          </div>
+        </div>
+        
+      </div>
+    </section>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { navLists } from '../constants';
 
 const Navbar = () => {
   return (
-    <header className='flex items-center justify-between w-full py-5 sm:px-10'>
+    <header className='flex items-center justify-between w-full px-5 py-5 sm:px-10'>
         <nav className="flex w-full screen-max-width">
             <img src={appleLogo} alt="Apple Image" width={14} height={18} className="" />
 
@@ -16,8 +16,8 @@ const Navbar = () => {
             </div>
 
             <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-                <img src={searchImg} alt="search" width={18} height={18} className="" />
-                <img src={bagImg} alt="search" width={18} height={18} className="" />
+                <img src={searchImg} alt="search" width={18} height={18} />
+                <img src={bagImg} alt="search" width={18} height={18} />
             </div>
         </nav>
     </header>
