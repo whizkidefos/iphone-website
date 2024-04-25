8f28099d9499da41a11c46a7180334987700faec
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-04-25 20:27:34 +0100

    Implemented Sentry for performance insight analysis

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7b887f9edbfb24d898e6c076c04348d4@o4507148699631616.ingest.de.sentry.io/4507148702253136",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
